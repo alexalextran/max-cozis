1317d54b980b06fed4588cbbb5ae86ffd5a32a48
Alex Tran <alextran8385@gmail.com>
2022-06-17 16:23:25 +1000

    plx integration for about me page

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -21,6 +21,44 @@ const AboutMe = () => {
           ]
         }
       ];
+
+      const aboutmeLeft = [
+        {
+          start: 'self',
+          duration: 500,
+          properties: [
+            {
+              startValue: 0,
+              endValue: 1,
+              property: "opacity"
+            },
+            {
+                startValue: -300,
+                endValue: 0,
+                property: "translateX"
+              },
+          ]
+        }
+      ];
+
+      const aboutmeRight = [
+        {
+          start: 'self',
+          duration: 500,
+          properties: [
+            {
+              startValue: 0,
+              endValue: 1,
+              property: "opacity"
+            },
+            {
+                startValue: 300,
+                endValue: 0,
+                property: "translateX"
+              },
+          ]
+        }
+      ];
       
 
 
@@ -28,18 +66,24 @@ const AboutMe = () => {
 
     return (
         <section className='aboutMe'>
-        <Plx parallaxData={exampleParallaxData}>
+        <Plx parallaxData={exampleParallaxData} className="plx">
             <p>"Every great design begins with an even better story." -- Lorinda Mamo</p>
             </Plx>
             <div className='aboutMe-div'>
+            <Plx parallaxData={aboutmeLeft}>
                 <div className='aboutme-left'>
                     <h1>Think Creatively</h1>
                     <p>Hey I'm max a visual and graphic artist who specialises in xyz, I developed a passion for art and design when i was very young and now I'm looking to make a career out of it</p>
                     <button>Download My Resume</button>
                 </div>
+                </Plx>
+
+                <Plx parallaxData={aboutmeRight}>
                 <div className='aboutme-right'>
                     <img src={aboutMe}></img>
                 </div>
+                </Plx>
+
             </div>
             
 
