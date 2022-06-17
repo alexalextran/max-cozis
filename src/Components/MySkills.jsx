4e44906dc69392d66dbcd0b230d24aa137fc9605
Alex Tran <alextran8385@gmail.com>
2022-06-17 20:31:10 +1000

    Skills_Learning complete

--- src/Components/MySkills.jsx
+++ src/Components/MySkills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {skills_proffecient} from '../Skills_Tech/index.js'
+import {skills_proffecient, skills_learning} from '../Skills_Tech/index.js'
 import SkillsCard from '../UI/SkillsCard';
 
 const MySkills = () => {
@@ -24,8 +24,14 @@ const MySkills = () => {
             </div>
             <div className='skills__right'>
             <h1>What I'm planning to learn</h1>
-          
-
+            <div className='cards'>
+            {
+                   skills_learning.map((skill, index) =>{
+                    let { Image } = skill;
+                    return <SkillsCard key={index} img={Image[skill.Name]} name={skill.Name} />
+                   }) 
+                }
+                </div>
             </div>
         </section>
     );
--- src/Skills_Tech/index.js
+++ src/Skills_Tech/index.js
@@ -2,6 +2,9 @@
  import Sketching from './images/pencil.svg'
  import Perspective from './images/perspective.svg'
  import ColorTheory from './images/ColorTheory.svg'
+ import Photoshop from './images/Photoshop.svg'
+ import Adobe_Illustrator from './images/Adobe_Illustrator.svg'
+ import Concept_Art from './images/Concept_Art.svg'
 
  
  const skills_proffecient = [
@@ -25,3 +28,24 @@
 
 
 export {skills_proffecient} 
+
+const skills_learning = [
+    {
+    Name: "Photoshop",
+    Image: {Photoshop}
+    },
+    {
+    Name: "Adobe_Illustrator",
+    Image: {Adobe_Illustrator}
+    },
+    {
+    Name: "Concept_Art",
+    Image: {Concept_Art}
+    },
+    {
+    Name: "idk",
+    Image: {ColorTheory}
+    }
+]
+
+export {skills_learning}
