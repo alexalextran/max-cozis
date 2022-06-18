61f82df24386fbda606b96a24d670024c4ea3e6e
Alex Tran <alextran8385@gmail.com>
2022-06-18 12:23:02 +1000

    text for the values section

--- src/Components/MyWork.jsx
+++ src/Components/MyWork.jsx
@@ -4,6 +4,7 @@ const MyWork = () => {
     return (
         <section className='mywork'>
             <h1>Some Text goes here</h1>
+            
         </section>
     );
 }
--- src/Components/Values.jsx
+++ src/Components/Values.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import Plx from "react-plx";
 
 const Values = () => {
+
+    const heading1 = [
+        {
+          start: 'self',
+          duration: '50vh',
+          properties: [
+            {
+              startValue: 0,
+              endValue: 1,
+              property: "opacity"
+            },
+            {
+                startValue: -300,
+                endValue: 0,
+                property: "translateX"
+              },
+          ]
+        }
+      ];
+
     return (
         <section className='values'>
-            text
+               
+               <Plx parallaxData={heading1}>
+               <h1>“The role of the designer is that of a good, thoughtful host anticipating the needs of his guests.” – Charles Eames, designer, architect and filmmaker </h1>
+               </Plx>
+
+               <Plx parallaxData={heading1}>
+               <h1>I believe good art isn't what comes forth from natural talent, but comes across from hard work and</h1>
+               </Plx>
+
+               <Plx parallaxData={heading1}>
+               <h1>Some text goes here, max you can add this later, got too lazy lol</h1>
+               </Plx>
+
         </section>
     );
 }
