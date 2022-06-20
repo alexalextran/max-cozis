efb444ce23ff51a444b0e5242873cae47546eba1
Alex Tran <alextran8385@gmail.com>
2022-06-20 20:52:47 +1000

    private route setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { AuthProvider } from "./Contexts/AuthContext";
 import {  Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
+import DashBoard from './Pages/DashBoard';
+import PrivateRoute from './PrivateRoute';
 function App() {
   return (
     <>
@@ -11,6 +13,9 @@ function App() {
     <Routes  basename={process.env.PUBLIC_URL}>
         <Route exact path="/" element={<HomePage />} />
         <Route path="login" element={<Login />} />
+        <Route  path="/dashboard" element={<PrivateRoute />}>
+              <Route  path="/dashboard" element={<DashBoard />} />
+            </Route>
       </Routes>
     </AuthProvider>
     </>
