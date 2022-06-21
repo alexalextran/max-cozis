7b19eca7be7b9676cbefd95ca6aa39c23f4c5c03
Alex Tran <alextran8385@gmail.com>
2022-06-21 18:41:00 +1000

    works section now reads off database and not file

--- src/Components/MyWork.jsx
+++ src/Components/MyWork.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slider from "react-slick";
 import "../Sass&Css/index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {worksobj} from '../Works/index.js'
-
+import { useState } from 'react';
+import { collection,getFirestore, getDocs  } from "firebase/firestore"; 
 
 const MyWork = () => {
+  const [works, setworks] = useState([])
+  const [loading, setloading] = useState(true)
+  const db = getFirestore();
+  const colRef = collection(db, 'works')
 
-
+useEffect(() => {
+  getDocs(colRef)
+  .then((snapshot) => {
+    
+      snapshot.docs.forEach((doc) => {
+       works.push({
+          ID: doc.id,
+           ...doc.data(),
+       })})
+      setloading(false)
+     
+      })
+      .catch(err => {
+          console.log(err.message)
+  })
+}, []);
+  
 
 
     var settings = {
@@ -22,13 +43,13 @@ const MyWork = () => {
             My works
         <Slider {...settings}>
          
-        {
-            worksobj.map((imgobj, index) =>{
+        {  loading ?  <p>Loading</p> :  
+            works.map((imgobj, index) =>{
                     return <div className='img' key={index}>
                         <p className='works__text'>
-                           {imgobj.desc}
+                           {imgobj.Description}
                         </p>
-                        <img src={imgobj.img[Object.keys(imgobj.img)[0]]}  alt="my drawings"></img>
+                        <img src={imgobj.Image}  alt="my drawings"></img>
                         </div>
                    }) 
                 }
